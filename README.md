41aeab24b75f9d1c46a6d2dec6e9266794e604ce
Siebe Van Dijck <siebe.vandijck@codegent.com>
2015-04-09 10:49:11 +0100

    finished basic structure

---
+++ README.md
@@ -0,0 +1,38 @@
+Gulp & Browserify Starter
+--------------------------
+
+A basic starter for gulp & browserify projects 
+
+
+## Including
+- Browserify (with .hbs support)
+- Handlebars
+- Watchify (for fast browserify )
+- Sass
+    - basic reset 
+    - skeleton (http://www.getskeleton.com)
+- Webserver
+
+
+## Get Started
+1. Install node ( & npm)
+2. Install gulp globally
+        npm install -g gulp
+3. Run Dependencies
+        npm install
+
+## Structure
+- build
+- src
+    - img
+    - js
+        - vendor
+    - sass
+    - index.html
+- gulpfile
+## Commands
+- the default task will startup a server at port 3000 (http://localhost:3000) and run everything unminified on every change you make to the file
+        gulp
+
+- the production task will clean out the build folder and compile/minify all the files (js,css,images), all the files in the src/js/vendor folder will be copied to build/js/vendor. 
+        gulp production
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 var browserify = require('browserify'),
     source = require('vinyl-source-stream'), // Transforming browserify so we can use it with gulp
-    buffer = require('vinyl-buffer');
+    watchify = require('watchify'),
+    buffer = require('vinyl-buffer'),
+    del = require('del'), // Clear out files & folders
     gulp = require('gulp'),
+    gutil = require('gulp-util'),
     sass = require('gulp-sass'),
     plumber = require('gulp-plumber'), // Fixes watch task on error 
     webserver = require('gulp-webserver'), // Server 
     notify = require('gulp-notify'), // Get Mac Notifications when a task is finished
-    clean = require('gulp-clean'), // Clear out files & folders
     imagemin = require('gulp-imagemin'),
     prefixer = require('gulp-autoprefixer'), // Prefix css with different browser stuff
     rename = require('gulp-rename'),
     minifyCSS = require('gulp-minify-css'),
     runSequence = require('run-sequence'), // Run tasks in sequence instead of parallel
     pngquant = require('imagemin-pngquant'),
+    assign = require('lodash.assign'),
     uglify = require('gulp-uglify');
 
 
@@ -39,22 +42,37 @@ gulp.task('styles', function() {
 });
 
 
-gulp.task('browserify', function () {
-  return browserify({
-    entries: './src/js/app.js',
-    extensions: ['.hbs'], 
-    debug:true
-  }).bundle()
+/*
+ * Browserify
+ */
+
+
+var browserifyOpts = {
+  entries: './src/js/app.js',
+  extensions: ['.hbs'], 
+  debug:true
+};
+
+var opts = assign({}, watchify.args, browserifyOpts);
+var b = watchify(browserify(opts));
+
+
+gulp.task('browserify',brwsrfy);
+b.on('update', brwsrfy);
+b.on('log', gutil.log); // output build logs to terminal
+
+
+function brwsrfy() {
+  return b.bundle()
     .on("error", notify.onError(function (error) {
       return "ERROR: " + error.message;
       this.emit('end');
     }))
-    .pipe(source('app.js'))
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('build/js'));
-});
-
-
+}
 
 
 /*
@@ -80,18 +98,13 @@ gulp.task('img',function(){
     .pipe(notify({ message: 'Image files moved' })); // Send notification
 });
 
-gulp.task('clean',function(){
-  return gulp.src('build/',{read: false})
-      .pipe(clean());
+gulp.task('clean',function(cb){
+    del(['build/**'], cb);
 });
 
 
 gulp.task('browserifyMin', function () {
-  return browserify({
-    entries: './src/js/app.js',
-    extensions: ['.hbs'], 
-    debug:true
-  }).bundle()
+  return browserify(browserifyOpts).bundle()
     .on("error", notify.onError(function (error) {
       return "ERROR: " + error.message;
       this.emit('end');
