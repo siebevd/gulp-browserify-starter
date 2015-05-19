0e2144371d34f139a9824b36cb69d483d71e6deb
Siebe Van Dijck <siebe.vandijck@codegent.com>
2015-05-19 16:54:26 +0100

    added in generated font with icons

--- gulpfile.js
+++ gulpfile.js
@@ -21,14 +21,22 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     prefixer = require('gulp-autoprefixer'), // Prefix css with different browser stuff
     minifyCSS = require('gulp-minify-css'),
+    combineMq = require('gulp-combine-mq'), // Combine all the media queries
+
+    iconfont = require('gulp-iconfont'),
+    consolidate = require('gulp-consolidate'),
 
     settings = {
-      DEST_BUILD : './build',
-      DEST_SRC : './src',
-      MAIN_JS : '/js/app.js',
-      MAIN_CSS : '/sass/style.scss',
-      COPY_FILE : ['src/*.html','src/js/vendor/*','src/img/**/**'],
-      PORT : 3000
+      DEST_BUILD : './build', // the path the the build folder
+      DEST_SRC : './src', // the path to the source folder
+      MAIN_JS : '/js/app.js', // the main javascript file
+      MAIN_CSS : '/sass/style.scss', // the main sass file
+      DIR_FONTS_SVG : '/icons/*.svg', // the path to the svg file to generate fonts
+      FONT_NAME : 'icnsfont', // the font name
+      FONT_CSS_TEMPLATE : '/sass/_tpl/icons.scss', // the template to generate font css
+      ICON_CLASS_NAME : 'icn', // the classname that will be given to the icons
+      COPY_FILE : ['src/*.html','src/js/vendor/*','src/img/**/**','src/fonts/**/**'], // the folders & files to copy (w/ watch) from src to build folder
+      PORT : 3000 // the port for the webserver
     };
 
 
@@ -39,7 +47,7 @@ gulp.task('webserver', function() {
   gulp.src(settings.DEST_BUILD)
     .pipe(webserver({
       fallback: 'index.html',
-      port: settings.port
+      port: settings.PORT
     }));
 });
 
@@ -73,7 +81,9 @@ function brwsrfy() {
     .pipe(source('app.js')) // Give the new file the name app.js
     .pipe(buffer()) // Transform it back to gulp readable stuff
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(settings.DEST_BUILD + '/js'));
+    .pipe(gulp.dest(settings.DEST_BUILD + '/js'))
+    .pipe(notify({ message: 'Browserify task complete' }));
+
 }
 
 gulp.task('brwsrfyMin',function() {
@@ -88,7 +98,7 @@ gulp.task('brwsrfyMin',function() {
     .pipe(stripDebug())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest(settings.DEST_BUILD + '/js'));
-}
+});
 
 
 
@@ -98,7 +108,7 @@ gulp.task('brwsrfyMin',function() {
 gulp.task('copyFiles',copyFiles);
 
 function copyFiles(){
-  return gulp.src(settings.COPY_FILE,{base:'src'})
+  return gulp.src(settings.COPY_FILE,{base:settings.DEST_SRC})
       .pipe(changed(settings.DEST_BUILD))
       .pipe(notify({ message: 'File Moved' }))
       .pipe(gulp.dest(settings.DEST_BUILD));
@@ -115,7 +125,7 @@ gulp.task('clean',function(cb){
 
 
 /*
- * Css
+ * Sass
  */
 
 gulp.task('styles', styles);
@@ -125,12 +135,38 @@ function styles() {
     .pipe(plumber({errorHandler: notify.onError("<%= error.fileName %> [<%= error.lineNumber %>]: <%= error.message %>")}))
     .pipe(sass())
     .pipe(prefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(combineMq())
     .pipe(rename({suffix: '.min'})) 
     .pipe(minifyCSS())
     .pipe(gulp.dest(settings.DEST_BUILD + '/css'))
     .pipe(notify({ message: 'Styles task complete' }));
 };
 
+/*
+ * Generate Icon Font
+ */
+gulp.task('iconFont',iconFont);
+
+function iconFont(){
+  gulp.src([settings.DEST_SRC + settings.DIR_FONTS_SVG])
+    .pipe(iconfont({
+      fontName: settings.FONT_NAME, // required
+      appendCodepoints: true // recommended option
+    }))
+      .on('codepoints', function(codepoints, options) {
+        // CSS templating, e.g.
+        gulp.src(settings.DEST_SRC + settings.FONT_CSS_TEMPLATE)
+          .pipe(consolidate('lodash', {
+            glyphs: codepoints,
+            fontName: settings.FONT_NAME,
+            fontPath: '../fonts/',
+            className: settings.ICON_CLASS_NAME
+          }))
+          .pipe(gulp.dest(settings.DEST_SRC + '/sass'));
+      })
+    .pipe(gulp.dest(settings.DEST_BUILD + '/fonts/'))
+    .pipe(notify({ message: 'fonts files generated' })); // Send notification
+}
 
 
 
@@ -138,11 +174,11 @@ function styles() {
  * Gulp Commands
  */
 
-gulp.task('default', ['webserver','copyFiles','styles','browserify','watch']);
+gulp.task('default', ['webserver','iconFont','copyFiles','styles','browserify','watch']);
 
 gulp.task('production',function(callback){
   // sequence it, so the clean one runs before all the rest
-  runSequence('clean',['styles','brwsrfyMin','copyFiles']);
+  runSequence('clean','iconFont',['styles','brwsrfyMin','copyFiles']);
 });
 
 
@@ -156,6 +192,9 @@ gulp.task('watch', function() {
   watch(settings.COPY_FILE,copyFiles);
 
   // Watch sass files
-  watch(settings.DEST_SRC + '/sass/*.scss',styles); 
+  watch(settings.DEST_SRC + '/sass/*.scss',styles);
+
+  // Watch svg files to convert to fonts
+  watch(settings.DEST_SRC + settings.DIR_FONTS_SVG ,iconFont);
 
 });
